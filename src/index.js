9051ef2d5732c83df0dcb98b78e3ab35dddf8f74
EstebanDev27 <esteban.galvan@redaliat.mx>
2025-10-17 09:46:48 -0600

    add: (Inicialicé el proyecto)

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import express from 'express';
+
+const app = express();
+const PORT = 3000;
+
+
+app.get('/', async (req, res) => {
+
+  try {
+
+    res.status(200).json({
+        message:"Ya estás conectado :D "
+    })
+    e
+
+  } catch (error) {
+
+    console.error('❌ Error en la ruta /:', error.message);
+    res.status(500).json({ error: 'Error interno del servidor.' });
+
+  } finally {
+    
+  }
+});
+
+
+app.listen(PORT, () => {
+  console.log(`🚀 Servidor Express escuchando en el puerto ${PORT}`);
+  console.log(`🔗 http://localhost:${PORT}`);
+});
