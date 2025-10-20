db9eb306243d33257b64ac038f2cebe3ecab6361
EstebanDev27 <esteban.galvan@redaliat.mx>
2025-10-20 10:09:51 -0600

    add: (Inicié implementación de colas y tareas)

---
+++ src/tasks/index.js
@@ -0,0 +1,46 @@
+import { CloudTasksClient } from '@google-cloud/tasks';
+
+// Instantiates a client.
+const client = new CloudTasksClient();
+
+async function createHttpTask() {
+  // TODO(developer): Uncomment these lines and replace with your values.
+  const project = 'agente-piloto';
+  const queue = 'dynamics-integration-queue';
+  const location = 'us-east1';
+  const url = '';
+  const payload = 'Adiós Mundo :C';
+  const inSeconds = 180;
+
+  // Construct the fully qualified queue name.
+  const parent = client.queuePath(project, location, queue);
+
+  const task = {
+    httpRequest: {
+      headers: {
+        'Content-Type': 'text/plain', // Set content type to ensure compatibility your application's request parsing
+      },
+      httpMethod: 'POST',
+      url,
+    },
+  };
+
+  if (payload) {
+    task.httpRequest.body = Buffer.from(payload).toString('base64');
+  }
+
+  if (inSeconds) {
+    // The time when the task is scheduled to be attempted.
+    task.scheduleTime = {
+      seconds: parseInt(inSeconds) + Date.now() / 1000,
+    };
+  }
+
+  // Send create task request.
+  console.log('Sending task:');
+  console.log(task);
+  const request = {parent: parent, task: task};
+  const [response] = await client.createTask(request);
+  console.log(`Created task ${response.name}`);
+}
+createHttpTask();
