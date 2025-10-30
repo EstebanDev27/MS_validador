4c79829a732493fa53b79fe8fc6bae30c9ab3152
EstebanDev27 <esteban.galvan@redaliat.mx>
2025-10-30 09:03:30 -0600

    add: (Correcion variables de entorno)

--- src/database/index.js
+++ src/database/index.js
@@ -1,7 +1,7 @@
 import { MongoClient } from "mongodb";
 
-const DB_NAME = process.env.DB_NAME; 
-const MONGO_URI = process.env.MONGO_URI;
+const DB_NAME = 'save-logs'; 
+const MONGO_URI = `mongodb://madmin:DonKara-{0127}@mongo-save-logs:27017/save-logs?authSource=admin`;
 
 const client = new MongoClient(MONGO_URI);
 
